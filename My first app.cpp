4eb260ad1f71cef86ec8f949dbd3aa0650c21074
ParentalUnit1of2 <90790291+ParentalUnit1of2@users.noreply.github.com>
2021-12-22 22:48:31 -0600

    Add project files.

---
+++ My first app.cpp
@@ -0,0 +1,14 @@
+// My first - first program
+
+#include <iostream>
+using namespace std; // so you don't have to write out std:: each time
+
+int main()
+{
+	cout << "Hello world!\n"; // This will output Hello world!
+
+	cout << "My name is JaNita." << std::endl;
+	cout << "Today is Tuesday\n";
+	cout << "Have a nice day";
+		system("pause>0"); // gets rid of the junk on the screen
+}
